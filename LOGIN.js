9b4daf0ddbe7c2fe510b1daf80a6d0c2aef4e35f
REZA-ARDANA-ROHMAN <ardanaaa815@gmail.com>
2025-12-23 11:46:05 +0700

    Add files via upload

---
+++ LOGIN.js
@@ -0,0 +1,16 @@
+function login() {
+  const user = document.getElementById('login-user').value;
+  const pass = document.getElementById('login-pass').value;
+  const err  = document.getElementById('login-error');
+
+  // === AKUN OPERATOR (STATIS) ===
+  const USERNAME = "operator";
+  const PASSWORD = "12345";
+
+  if (user === USERNAME && pass === PASSWORD) {
+    localStorage.setItem("loginOK", "true");
+    window.location.href = "singlefile_webapp.html"; // halaman dashboard
+  } else {
+    err.innerText = "Username atau Password salah";
+  }
+}
